c930f19e22d350e13e20a07f0e4e800de6b22da5
vivgopal <vivek.gopal.gv@gmail.com>
2017-04-17 00:02:55 -0700

    add basic spinner in xml and Java

--- app/src/main/java/com/example/vivekgopal/project1/MainActivity.java
+++ app/src/main/java/com/example/vivekgopal/project1/MainActivity.java
@@ -1,4 +1,14 @@
 package com.example.vivekgopal.project1;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.content.Intent;
 import android.view.Menu;
 import android.graphics.Typeface;
@@ -32,31 +42,79 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        //setContentView(R.layout.linear_layout_screen1);
+        // Initialize spinners
+        Spinner dynamicSpinnerAge = (Spinner) findViewById(R.id.dynamic_spinner_age);
+        Spinner dynamicSpinnerPresent = (Spinner) findViewById(R.id.dynamic_spinner_present);
+        Spinner dynamicSpinnerFuture = (Spinner) findViewById(R.id.dynamic_spinner_future);
 
-        //layout_age_19_22 = (LinearLayout) findViewById(R.id.linear_layout_screen_age_19_23);
-        //Button button_age_19_22 = (Button) findViewById(R.id.button_age_19_22);
-        //button_age_19_22.setOnClickListener(new OnClickListener() {
-        //
-        //    @Override
-        //    public void onClick(View v) {
-        //        LayoutInflater inflater = LayoutInflater
-        //                .from(getApplicationContext());
-        //        View view = inflater.inflate(R.layout.linear_layout_screen_age_19_22, null);
-        //        layout_age_19_22.addView(view);
-        //
-        //    }
-        //});
+        // Spinner array adapter strings
+        String[] ageItems = new String[] { "14 - 15", "16 - 18", "19 - 22", "23+" };
+        String[] presentItems = new String[] { "10th", "12th", "Diploma", "1st Year" };
+        String[] futureItems = new String[] { "Work", "Study" };
+
+        // Set spinner prompt
+        //dynamicSpinnerAge.setPrompt("Select Age");
+        //dynamicSpinnerPresent.setPrompt("Currently I do");
+        //dynamicSpinnerFuture.setPrompt("I want to");
+
+        // Array adapter initialization
+        ArrayAdapter<String> adapterAge = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, ageItems);
+        ArrayAdapter<String> adapterPresent = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, presentItems);
+        ArrayAdapter<String> adapterFuture = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, futureItems);
+
+        // set adapter
+        dynamicSpinnerAge.setAdapter(adapterAge);
+        dynamicSpinnerPresent.setAdapter(adapterPresent);
+        dynamicSpinnerFuture.setAdapter(adapterFuture);
+
+        dynamicSpinnerAge.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        dynamicSpinnerPresent.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
+
+        dynamicSpinnerFuture.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                Log.v("item", (String) parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
 
     }
 
     // Called when the user clicks the Send button
-    public void open_activity_19_22(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, LayoutScreenAge19to22Activity.class);
-        //EditText editText = (EditText) findViewById(R.id.edit_message);
-        //String message = editText.getText().toString();
-        //intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+//    public void open_activity_19_22(View view) {
+//        // Do something in response to button
+//        Intent intent = new Intent(this, LayoutScreenAge19to22Activity.class);
+//        //EditText editText = (EditText) findViewById(R.id.edit_message);
+//        //String message = editText.getText().toString();
+//        //intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
 }
