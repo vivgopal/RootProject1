d8c6fcc53769507be614f1c379214cf5258346b7
vivgopal <vivek.gopal.gv@gmail.com>
2017-10-11 00:01:13 -0700

    Added new CertificationListAdapter and modified DisplayCertificationsActivity to programatically add certifications based on content from DB

--- app/src/main/java/com/example/vivekgopal/project1/activities/DisplayCertificationsActivity.java
+++ app/src/main/java/com/example/vivekgopal/project1/activities/DisplayCertificationsActivity.java
@@ -9,12 +9,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.vivekgopal.project1.R;
+import com.example.vivekgopal.project1.adapters.CertificationListAdapter;
+import com.example.vivekgopal.project1.data.CertificationItem;
 import com.example.vivekgopal.project1.data.SalaryItem;
 
 import org.apache.commons.lang3.text.WordUtils;
@@ -25,11 +30,10 @@ import java.util.List;
 
 public class DisplayCertificationsActivity extends GenericDbActivity {
 
-    LinearLayout container;
-    List<LinearLayout> certificationList = new ArrayList<>();
-    
-    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
-    String[] items = {""};
+    ListView list;
+    String[] certificationTitle;
+    Integer[] certificationLogoId;
+    List<CertificationItem> certificationItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,27 +43,27 @@ public class DisplayCertificationsActivity extends GenericDbActivity {
         subtitleTextView.setText(subtitle + " | Certifications");
 
         openDatabase();
-        items = mDbAdapter.getCompanies(WordUtils.uncapitalize(title), WordUtils.uncapitalize(subtitle)).toArray(items);
-        Arrays.sort(items);
+        certificationItemList = mDbAdapter.getCertifications(WordUtils.uncapitalize(title), WordUtils.uncapitalize(subtitle));
         closeDatabase();
 
-        container = (LinearLayout) findViewById(R.id.activity_certifications_container);
-        LayoutInflater inflater =  LayoutInflater.from(this.getApplicationContext());
-
-        params.setMargins(5, 5, 5, 5);
-
-        for(int i=0; i<items.length; i++) {
-            View certificationLayout = inflater.inflate(R.layout.layout_certification, null, false);
-            certificationLayout.setLayoutParams(params);
-            container.addView(certificationLayout);
+        certificationTitle = new String[certificationItemList.size()];
+        certificationLogoId = new Integer[certificationItemList.size()];
+        int i = 0;
+        for(CertificationItem item : certificationItemList){
+            certificationTitle[i] = item.getName();
+            certificationLogoId[i] = getResources().getIdentifier("logo_" + WordUtils.uncapitalize(item.getSource()), "drawable", getPackageName());
+            i++;
         }
-//        for (String item : items) {
-//            View certificationLayout = inflater.inflate(R.layout.layout_certification, null, false);
-//            ((View) certificationLayout.findViewById(R.id.certification_title))setText(item.getCompany());
-//            ((View) certificationLayout.findViewById(R.id.row_value)).setText("₹" + item.getSalary());
-//            row.setPadding(5, 5, 5, 5);
-//            table.addView(row);
-//        }
+        CertificationListAdapter adapter = new CertificationListAdapter(DisplayCertificationsActivity.this, certificationTitle, certificationLogoId);
+        list=(ListView)findViewById(R.id.activity_certifications_list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(DisplayCertificationsActivity.this, "You Clicked at " +certificationTitle[+ position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
 
